ec13086e5591264010f87acaaac00dba7a967cd8
Braian-Zignago <braianzig77@gmail.com>
2025-07-23 22:04:08 -0300

    feat: Create Controller to / 'mensage welcome'

---
+++ src/main/java/java10x/com/CadastroDeNinjas/Controller/Controller.java
@@ -0,0 +1,15 @@
+package java10x.com.CadastroDeNinjas.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class Controller {
+
+    @GetMapping
+    public String boasVindas() {
+        return "Bem-vindo ao Cadastro de Ninjas!";
+    }
+}
