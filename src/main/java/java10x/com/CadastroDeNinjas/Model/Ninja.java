dca61431960946d00310175ad5e72bb92511ae93
Braian-Zignago <braianzig77@gmail.com>
2025-07-23 22:38:35 -0300

    feat: Create Entity Ninja

--- src/main/java/java10x/com/CadastroDeNinjas/Model/Ninja.java
+++ src/main/java/java10x/com/CadastroDeNinjas/Model/Ninja.java
@@ -1,4 +1,56 @@
-package java10x.com.CadastroDeNinjas.domain;
+package java10x.com.CadastroDeNinjas.Model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
 public class Ninja {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    public Ninja() {
+    }
+
+    public Ninja(int idade, String email, String nome) {
+        this.idade = idade;
+        this.email = email;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
