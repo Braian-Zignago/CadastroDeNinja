6194bfa50654006e55a76ca4013cadcfd9dcd1be
Braian-Zignago <braianzig77@gmail.com>
2025-08-05 20:52:19 -0300

    fix: change class MissoesRepository and NinjaRepositoy into interface

---
+++ src/main/java/java10x/com/CadastroDeNinjas/missoes/MissoesRepository.java
@@ -0,0 +1,6 @@
+package java10x.com.CadastroDeNinjas.missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
+}
