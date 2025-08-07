ab8f52530998476c06fb47c73ae96d7f22620880
Braian-Zignago <braianzig77@gmail.com>
2025-08-06 21:05:01 -0300

    chore: modific .gitignore

--- src/main/java/java10x/com/CadastroDeNinjas/missoes/MissoesModel.java
+++ src/main/java/java10x/com/CadastroDeNinjas/missoes/MissoesModel.java
@@ -1,6 +1,7 @@
 package java10x.com.CadastroDeNinjas.missoes;
 
 import jakarta.persistence.*;
+import java10x.com.CadastroDeNinjas.Ninjas.NinjaModel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,5 +24,5 @@ public class MissoesModel {
 
     // Relacionamento com a tabela de ninjas: Missao pode ter vários ninjas nela.
     @OneToMany(mappedBy = "missoes") // mappedBy indica que a relação é gerenciada pela classe NinjaModel
-    private List<String> ninjas;
+    private List<NinjaModel> ninjas;
 }
